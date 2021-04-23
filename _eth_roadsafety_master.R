cee5bd455c9526eaa29be4cd2597678c6a1d694a
Rob Marty <ramarty@email.wm.edu>
2021-04-23 16:00:52 -0400

    update master

--- _eth_roadsafety_master.R
+++ _eth_roadsafety_master.R
@@ -18,6 +18,7 @@ dailyhourly_dir  <- file.path(data_dir, "Daily and Hourly Data")
 
 ## Github Directories
 functions_dir <- file.path(github_dir, "Functions")
+datawork_dir <- file.path(github_dir, "DataWork")
 
 # Packages ---------------------------------------------------------------------
 library(sf)
@@ -47,20 +48,27 @@ source(file.path(functions_dir, "turning_angle.R"))
 # Run Scripts ------------------------------------------------------------------
 if(F){
 
-  # Prepare Data ---------------------------------------------------------------
-  # Creates precipitation dataset
-  daily_precipitation.R
+  # Clean Individual Datasets --------------------------------------------------
+  ## Addis Adama Expressway (Polyline and Points)
+  source(file.path(datawork_dir, "Addis Ababa Expressway", "01_clean_expressway", "01_create_expressway_file.R"))
+  source(file.path(datawork_dir, "Addis Ababa Expressway", "01_clean_expressway", "02_create_points_along_expressway.R"))
   
-  # Create single dataset with all accidents / traffic
-  append_clean_accident_data.R
-  append_clean_traffic_data.R
+  ## ETRE Data (Crashes and Traffic)
+  source(file.path(datawork_dir, "ETRE - Crashes", "clean_crashes.R"))
+  source(file.path(datawork_dir, "ETRE - Traffic", "append_clean_traffic_data.R"))
   
-  create_accident_segment_dataset.R
-  create_accident_traffic_hourly_daily_datasets.R
-  create_traffic_segment_dataset.R
+  ## Precipitation near expressway
+  source(file.path(datawork_dir, "Precipitation", "daily_precipitation.R"))
   
-  # Analysis -------------------------------------------------------------------
-  figures_maps.R
-
+  # Daily/Hourly Data and Analysis ---------------------------------------------
+  ## Clean Data
+  source(file.path(datawork_dir, "Daily and Hourly Data", "01_create_daily_hourly_daily_datasets.R"))
+  
+  # Segment Level Data and Analysis --------------------------------------------
+  ## Clean Data
+  source(file.path(datawork_dir, "Addis Ababa Expressway", "02_add_data_to_points", "01_crashes.R"))
+  source(file.path(datawork_dir, "Addis Ababa Expressway", "02_add_data_to_points", "01_dist_toll_plazas.R"))
+  source(file.path(datawork_dir, "Addis Ababa Expressway", "02_add_data_to_points", "01_turn_angle.R"))
+  source(file.path(datawork_dir, "Addis Ababa Expressway", "02_add_data_to_points", "02_merge_clean.R"))
 }
 
